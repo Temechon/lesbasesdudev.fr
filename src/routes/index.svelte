43f21f483864be00d59eb4c27cfd83869e185bdb
Julian <julian.chenard@gmail.com>
2022-07-16 22:47:32 +0200

    First version - with Svelte <3

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "../app.css";
+</script>
+
+<section class="w-2/3 m-auto text-center mt-20">
+    <h1 class="my-10 text-8xl font-extrabold ">
+        Apprendre les bases du <span
+            class="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-pink-600"
+            >développement web</span
+        >
+    </h1>
+    <h2 class="text-3xl my-36">
+        Plus de 200 pages, 10 chapitres et plus de 40 exercices corrigés pour
+        apprendre<br />
+        <span class="font-bold">les bases du développement web.</span>
+    </h2>
+
+    <button class="bg-pink-500 px-7 py-4 text-white text-xl rounded-xl">
+        Ca m'intéresse !</button
+    >
+</section>
